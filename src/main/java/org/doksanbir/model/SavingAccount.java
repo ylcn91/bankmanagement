fa7c21796a4808715cc094dcd729502a84ae4a30
yalcindoksanbir <yalcin.doksanbir@emlakjet.com>
2024-10-08 19:47:04 +0300

    added saving account

---
+++ src/main/java/org/doksanbir/model/SavingAccount.java
@@ -0,0 +1,8 @@
+package org.doksanbir.model;
+
+import org.doksanbir.interfaces.Intereset;
+
+//TODO: Implement the Intereset interface
+public class SavingAccount extends Account implements Intereset {
+
+}
